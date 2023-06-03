40b4700f0b4a00636ec00f0f70d41d0e7c64915a
Mr.Jack <133187909+MrLuohao@users.noreply.github.com>
2023-06-03 22:17:37 +0800

    Update testUpdateGit.java

--- SpringAOPTest1/src/main/java/studuy/test/testUpdateGit.java
+++ SpringAOPTest1/src/main/java/studuy/test/testUpdateGit.java
@@ -3,5 +3,6 @@ package studuy.test;
 public class testUpdateGit {
     public static void main(String[] args) {
         System.out.println("本地仓库更新了testUpdateGit文件");
+        System.out.println("GitHub仓库更新了testUpdateGit文件");
     }
 }
