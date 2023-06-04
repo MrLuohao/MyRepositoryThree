dc4afdd2a548957e97917f562023fc45cb5b50f5
‘LuoHao’ <1441432393@qq.com>
2023-06-04 15:55:12 +0800

    Default Changelist

--- SpringAOPTest1/src/main/java/studuy/test/testUpdateGit.java
+++ SpringAOPTest1/src/main/java/studuy/test/testUpdateGit.java
@@ -4,5 +4,6 @@ public class testUpdateGit {
     public static void main(String[] args) {
         System.out.println("本地仓库更新了testUpdateGit文件");
         System.out.println("GitHub仓库更新了testUpdateGit文件");
+        System.out.println("本地仓库更新了文件");
     }
 }
